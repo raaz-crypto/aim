9279a45cd3482b696cf7da6fc6f92285d1257842
rabisg <guha.rabishankar@gmail.com>
2013-10-22 21:59:07 +0530

    Replace `:=>` by `Supports`

--- Aim/Feature.hs
+++ Aim/Feature.hs
@@ -13,47 +13,29 @@ features that might be supported by the architecture (e.g.  SSE). This
 module facilitates capturing such constraints at the type level.
 
 The constraints that we support are conjunctions of what are instances
-of the class `Feature`. The type class `:=>:` then captures the
-implication relation on conjunctions of features.
+of the class `Feature`. The type class `Supports` then captures the
+membership relation on conjunctions of features.
 
 -}
 
 module Aim.Feature
        ( Feature
-       , (:=>:)
+       , Supports
        , (:/\:)
        ) where
 
 -- | A class that captures all basic features. Example for features
--- are assertions like the architecture is X86 or something similar.
+-- are assertions like hasAESNI or SSE or something similar.
 class Feature feature
 
 -- | A conjunction type. This is used to capture a conjunction of
 -- features.
 data feature :/\: conjunction
 
--- | A constraint @conjA `:=>:` conjB@ captures implies relation among
--- a conjuctions @conjA@ and @conjB@. Assume that we have already
--- defined a feature instance for the types @Arch X86@ and @SSE@. We
--- can use the `:=>:` to /restrict/ (at the type level) the
--- instruction @instr@ to all X86 architecture with support for SSE as
--- follows:
---
--- > instr :: arch :=>: Arch X86 :/\: SSE
--- >       => ...
--- >
-class conjA :=>: conjB
+-- | A constraint @Supports conj feature@ captures the membership relation
+-- among a set of features(represented by @conj@) and a @feature@.
+class (Feature feature) => Supports conj feature
 
-infixr 9 :/\:
-infixr 8 :=>:
+instance Feature feature => Supports feature feature
 
-instance Feature feature => feature :=>: feature
-
-instance Feature feature => feature :/\: conj :=>: feature
-
-instance ( Feature feature
-         , conjA :=>: conjB
-         , conjA :=>: feature
-         )
-         =>
-         conjA :=>: feature :/\: conjB
+instance (Feature feature, Supports conjA feature) => Supports (conjA :/\: conjB) feature
