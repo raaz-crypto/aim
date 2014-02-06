e603a8941a5c4f4ec1f24cb94dfd8ac3f9d8d877
Piyush P Kurur <ppk@cse.iitk.ac.in>
2014-02-06 12:39:01 +0530

    Aim.Machines bothers only of machines now
    The actual assembly language now is delegated to
    Aim.Assembler.Language.

--- Aim/Machine.hs
+++ Aim/Machine.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE MultiParamTypeClasses  #-}
 {-# LANGUAGE FlexibleContexts       #-}
 {-# LANGUAGE OverloadedStrings      #-}
+{-# LANGUAGE UndecidableInstances   #-}
 {-|
 
 One of the important consideration in the design of Aim is that errors
@@ -22,105 +23,82 @@ module Aim.Machine
        (
        -- * Architecture and Machine.
          Arch, Machine(..)
-       , Supports
-       , Instruction, Instructions, assembleInstructions
-       , comments, (<#>)
        -- * Basic machine types
-       -- $basicmachinetypes$
-       , Type8Bits, Type16Bits, Type32Bits, Type64Bits, Type128Bits
+       -- $machinetypes$
+       , WordSize
+       , MachineType(..)
+       , Supports
        , Register
        , Operand (..)
        ) where
 
 import GHC.Exts         ( Constraint                    )
-import Data.Text        ( Text, unlines, lines, length
-                        , replicate, null
-                        )
 import Data.Word        ( Word8, Word16, Word32, Word64 )
 import Data.Int         ( Int8,  Int16,  Int32,  Int64  )
-import Data.Monoid
-import Data.String      ( IsString(..)                  )
-import Prelude hiding   ( unlines, lines, length
-                        , replicate, null
-                        )
 
+import Aim.Assembler.Language ( Size(..) )
 
-
+-- | Class that captures an architecture.
 class Arch arch
 
 -- | Class that captures a machine.
 class Arch (ArchOf machine) => Machine machine where
-  type ArchOf machine :: *
+  type ArchOf machine  :: *
 
--- | Whether the machine supports values of this type.
-class Machine machine => Supports machine typ
-
-data Instruction machine = Instruction Text
-
-instance IsString (Instruction machine) where
-  fromString = Instruction . fromString
-
--- | A sequence of instructions.
-type Instructions machine = [Instruction machine]
-
--- | Assemble a sequence of instructions into the corresponding
--- assembly language program.
-assembleInstructions :: Instructions machine -> Text
-assembleInstructions = unlines . map text
-  where text (Instruction txt) = txt
-
--- | Generate a comment block
-comments :: Text -> Instructions machine
-comments txt = [ Instruction $ "# " <> t | t <- lines txt ]
-
--- | Comment on an instruction
-(<#>) :: Instruction machine -- ^ The instruction to comment on
-      -> Text                -- ^ The comment
-      -> Instruction machine
-(<#>) inst@(Instruction txt) com
-  | null com  =  inst
-  | otherwise = Instruction $ unlines $ first : rest
-  where pad    = replicate (length txt + 3) " "
-        (c:cs) = lines com
-        first  = txt <> replicate 3 " " <> "# " <> c
-        rest   = [pad <> "# " <> x | x <- cs]
-
--- $basicmachinetypes$
+-- $machinetypes$
 --
--- In assembly language, the basic units of information 8-bit, 16-bit,
--- 32-bit, 64-bit and sometimes 128-bit signed and unsigned
--- integers. However, to improve type safety, we should be able to use
--- opaque types that can be encoded as one of the above types. For
--- example, we would want to treat Char8 differently than Word8 even
--- though they are encoded similary on the machine. We support using
--- any of these opaque types in assembly instructions. The classes
--- `Type8Bits`, `Type16Bits`, `Type32Bits`, `Type64Bits` and
--- `Type128Bits` are meant to capture these opaque types.
+-- In assembly language, we have 8-bit, 16-bit, 32-bit, 64-bit and
+-- sometimes 128-bit and 256-bit signed and unsigned integral
+-- values. Depending on the word size of the processor, some of these
+-- types can be loaded on general purposes registers of the
+-- machine. However, we want the code written in aim to be as type
+-- safe as possible. We allow the use opaque types that can be encoded
+-- as one of the above types. For example, we would want to treat
+-- `Char8` differently than `Word8` even though they are encoded
+-- similary on the machine. We capture this type safety by three
+-- classes `MachineType` for types, `WordSize` for machines. For every
+-- type that we want to process by some machine, we declare it to be
+-- an instance of the class `MachineType`. Similarly, for each machine
+-- we would like to define which sizes they support. This is captured
+-- by the class `WordSize`. Finally, the constraint, @`Support`
+-- machine ty@ captures whether a machine supports the type @ty@
 
--- | Types that are essentially 8-bit quantities.
-class Type8Bits typ
 
-instance Type8Bits Word8; instance Type8Bits Int8
+-- | Declaring a type to be an instance of `MachineType` means that it
+-- can potentially be stored and processed in some machines registers.
+-- The Associated type `TypeSize` captures the Size required to
+-- capture the given type @ty@
+class MachineType ty where
+  type TypeSize ty :: Size
 
--- | Types that are essentially 16-bit quantities.
-class Type16Bits typ
-instance Type16Bits Word16; instance Type16Bits Int16
+instance MachineType  Word8 where type TypeSize Word8  = Size8
+instance MachineType   Int8 where type TypeSize  Int8  = Size8
 
--- | Types that are essentially 32-bit quantities
-class Type32Bits typ
-instance Type32Bits Word32; instance Type32Bits Int32
+instance MachineType Word16 where type TypeSize Word16 = Size16
+instance MachineType  Int16 where type TypeSize  Int16 = Size16
 
--- | Types that are essentially 64-bit quantities
-class Type64Bits typ
-instance Type64Bits Word64; instance Type64Bits Int64
+instance MachineType Word32 where type TypeSize Word32 = Size32
+instance MachineType  Int32 where type TypeSize  Int32 = Size32
 
--- | Types that are essentially 128-bit quantities
-class Type128Bits typ
+instance MachineType Word64 where type TypeSize Word64 = Size64
+instance MachineType  Int64 where type TypeSize  Int64 = Size64
 
--- | Class that captures all possible operands. Operands have a type
--- and they are usually suppored on a specific architecture. They also
--- put additional constraints on the underlying machine.
-class Arch (SupportedOn operand) => Operand operand where
+
+-- | The instance `WordSize mach sz` means that the machine `mach` can
+-- process integral valuse of size sz.
+class WordSize machine (sz :: Size)
+
+
+-- | Supports is the constraint type that captures when a machine
+-- supports a given machine type ty
+type Supports machine ty = ( MachineType ty
+                           , WordSize machine (TypeSize ty)
+                           )
+
+-- | An operand of a machine.
+class ( Arch (SupportedOn operand)
+      , MachineType (Type operand)
+      ) => Operand operand where
 
   -- | The architecture on which this operand is supported.
   type SupportedOn operand :: *
@@ -130,12 +108,14 @@ class Arch (SupportedOn operand) => Operand operand where
 
   -- | The constraint on the machine for this operand to exist. The
   -- default constraint is that the machine and the register have the
-  -- same underlying arch.
+  -- same underlying arch and the type of the operand is supported by
+  -- the machine.
   type MachineConstraint machine operand :: Constraint
 
-  type MachineConstraint machine operand = ( Supports machine (Type operand)
-                                           , SupportedOn operand ~ ArchOf machine
-                                           )
+  type MachineConstraint machine operand
+          = ( Supports machine (Type operand)
+            , SupportedOn operand ~ ArchOf machine
+            )
 
 -- | Constraint that the given operand is a register.
 class Operand operand => Register operand
