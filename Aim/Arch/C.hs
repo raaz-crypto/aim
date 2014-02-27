ff0a1c2ad5b28f2bcdb4ead8082bd5c31bf71373
Piyush P Kurur <ppk@cse.iitk.ac.in>
2014-02-27 19:13:09 +0530

    added portable C architecture

---
+++ Aim/Arch/C.hs
@@ -0,0 +1,35 @@
+{-|
+
+This module supports generation of C Code via aim.
+
+-}
+{-# LANGUAGE DataKinds              #-}
+{-# LANGUAGE GADTs                  #-}
+{-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE TypeFamilies           #-}
+{-# LANGUAGE EmptyDataDecls         #-}
+
+module Aim.Arch.C
+       ( C
+       ) where
+
+import Aim.Machine
+
+-- | The portable C architecture.
+data C
+
+-- | C is an instance of Arch.
+instance Arch C
+
+-- | There is only one possible machine for the C architecture.
+instance Machine C where
+  type ArchOf C = C
+
+-- | C supports  8-bit integrals.
+instance WordSize C Size8
+-- | C supports 16-bit integrals.
+instance WordSize C Size16
+-- | C supports 32-bit integrals.
+instance WordSize C Size32
+-- | C supports 64-bit integrals.
+instance WordSize C Size64
