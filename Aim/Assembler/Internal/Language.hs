77f7e2eecf95aa1476229b202667dd915c1f541a
Piyush P Kurur <ppk@cse.iitk.ac.in>
2014-05-10 17:02:59 +0530

    internal assembly language works only with unsigned stuff

--- Aim/Assembler/Internal/Language.hs
+++ Aim/Assembler/Internal/Language.hs
@@ -16,9 +16,8 @@ module Aim.Assembler.Internal.Language
        , Statement(..), Arg(..), VarDec(..)
        -- * Helpers to create immediate arguments
        , word8, word16, word32, word64, word128, word256, char8
-       , int8, int16, int32, int64, int128, int256
        -- * Constants
-       , Constant(..), Signed(..), signed, unsigned
+       , Constant(..)
        -- * Stuff with comments
        , Commented(..), (<#>), (<!>)
        -- * Some Monoids
@@ -119,78 +118,39 @@ data VarDec = VarDec (Signed Size) Text deriving Show
 
 -- | An 8-bit unsigned integer.
 word8  :: Word8  -> (Arg arch)
-word8  = Immediate . I Size8 . unsigned . toInteger
+word8  = Immediate . I Size8 . toInteger
 
 -- | A 16-bit unsigned integer.
 word16 :: Word16 -> (Arg arch)
-word16 = Immediate . I Size16 . unsigned . toInteger
+word16 = Immediate . I Size16 . toInteger
 
 -- | A 32-bit unsigned integer.
 word32 :: Word32 -> (Arg arch)
-word32 = Immediate . I Size32 . unsigned . toInteger
+word32 = Immediate . I Size32 . toInteger
 
 -- | A 64-bit unsiged integer.
 word64 :: Word64 -> (Arg arch)
-word64 = Immediate . I Size64 . unsigned . toInteger
+word64 = Immediate . I Size64 . toInteger
 
 -- | A 128-bit unsigned integer.
 word128 :: Integer -> (Arg arch)
-word128 = Immediate . I Size128 . unsigned
+word128 = Immediate . I Size128
 
 -- | A 256-bit unsiged integer.
 word256 :: Integer -> (Arg arch)
-word256 = Immediate . I Size256 . unsigned
+word256 = Immediate . I Size256
 
 -- | Encode a character into its ascii equivalent.
 char8 :: Char -> (Arg arch)
 char8 = word8 . toEnum . fromEnum
 
--- | A signed 8-bit integer
-int8 :: Int8 -> (Arg arch)
-int8 = Immediate . I Size8 . signed . toInteger
-
--- | A signed 16-bit integer
-int16 :: Int16 -> (Arg arch)
-int16 = Immediate . I Size16 . signed . toInteger
-
--- | A signed 32-bit integer
-int32 :: Int32 -> (Arg arch)
-int32 = Immediate . I Size32 . signed . toInteger
-
--- | A signed 64-bit integer
-int64 :: Int64 -> (Arg arch)
-int64 = Immediate . I Size64 . signed . toInteger
-
--- | A signed 128-bit integer.
-int128 :: Integer -> (Arg arch)
-int128 = Immediate . I Size128 . signed
-
--- | A signed 256-bit integer.
-int256 :: Integer -> (Arg arch)
-int256 = Immediate . I Size256 . signed
-
-
 --------------------- Constants ----------------------------------------
 
 
 -- | A constant.
-data Constant = I Size (Signed Integer)  -- ^ A signed integer
-              | F Double                 -- ^ A floting point constant.
+data Constant = I Size Integer  -- ^ A signed integer
+              | F Double        -- ^ A floting point constant.
               deriving Show
-
-
--- | Tags the value to distinguish between signed and unsigned
--- quantities.
-data Signed a = S a
-              | U a  deriving Show
-
--- | Create a signed object.
-signed :: a -> Signed a
-signed = S
-
--- | Create an unsigned object.
-unsigned :: a -> Signed a
-unsigned =  U
 
 ------------------ Commenting ------------------------------------------
 
